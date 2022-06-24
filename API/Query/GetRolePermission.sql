0215ea9ef5a7e0530073816e210e0ae6a2f881c4
sanank <sanankr@icloud.com>
2022-06-24 12:48:24 +0530

    modified and added query

--- API/Query/GetRolePermission.sql
+++ API/Query/GetRolePermission.sql
@@ -1,8 +1,8 @@
-SELECT RolePermission.Role,
-    RolePermission.Fetch,
-    RolePermission.Create,
-    RolePermission.Update,
-    RolePermission.Delete,
+SELECT RolePermission.Role As "role",
+    RolePermission.PermFetch As "fetch",
+    RolePermission.PermCreate As "create",
+    RolePermission.PermUpdate As "update",
+    RolePermission.PermDelete As "delete",
 FROM RolePermission
 WHERE RolePermission.Role = @Role
 
