7bf1422e947f34bca08796d42164d25bd4561992
sanank <sanankr@icloud.com>
2022-06-24 20:07:42 +0530

    added script to setup database and required tables

---
+++ Database/script.sql
@@ -0,0 +1,33 @@
+ Create database OneDotECom;
+
+ -- use OneDotECom
+
+Create Table RolePermission(
+	Id Int Auto_Increment Primary Key,
+    Role varchar(32) Unique Not Null,
+    PermFetch Boolean Default False,
+    PermCreate Boolean Default False,
+    PermUpdate Boolean Default False,
+    PermDelete Boolean Default False
+);
+
+
+Create Table User(
+	Id Int Auto_Increment Primary Key,
+    UserName varchar(16) Unique Not Null,
+    Password varchar(128) Not Null,
+    Role Varchar(25) Not Null,
+    Foreign Key(Role) References RolePermission(Role)
+);
+
+Insert Into RolePermission(
+	Role, PermFetch, PermCreate, PermUpdate, PermDelete
+) Values
+
+	("Admin", true, true, true, true),
+    ("Seller", true, true, true, false),
+    ("Supporter", true,false, false, true),
+    ("Customer", true, false, false, false)
+;
+
+
