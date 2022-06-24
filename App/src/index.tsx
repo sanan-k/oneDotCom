39e1c8ec6c4aa532c32bea62921ef95ed8534525
sanank <sanankr@icloud.com>
2022-06-25 03:16:34 +0530

    fix import path

--- App/src/index.tsx
+++ App/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Auth from "./container/Auth";
+import Auth from "./container/auth";
 import Home from "./container/home";
 
 const appCntnrStyle: React.CSSProperties = {
