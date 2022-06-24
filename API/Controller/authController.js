91c8cacd4b735cf5359a613ea009af819d20115b
sanank <sanankr@icloud.com>
2022-06-24 14:24:04 +0530

    added login and logout functionality

--- API/Controller/authController.js
+++ API/Controller/authController.js
@@ -1,5 +1,48 @@
-const login = async (ctx) => {};
+const { getUser } = require("../Service/userService");
+const { hash } = require("../Helper/encryption");
+const { generateJWT } = require("../Service/AuthService");
+const {
+  createSession,
+  clearSession,
+  checkSession,
+} = require("../Service/Session");
+
+const { sys_messages } = require("../config.json");
+
+const login = async (ctx) => {
+  const { userName, password } = ctx.params;
+  const user = await getUser(userName);
+  const hashedPassword = hash(password);
+
+  if (hashedPassword === user.password) {
+    const token = await generateJWT({ userName, role: user.role });
+    const sessionCreated = createSession(token, user.role);
+    if (sessionCreated) {
+      return token;
+    }
+
+    throw Error(sys_messages.error.SESSION_NOT_CREATED);
+  }
+
+  throw Error(sys_messages.error.INVALID_CREDENTIAL);
+};
+
+const logout = (ctx) => {
+  const { token } = ctx.auth;
+
+  if (checkSession(token)) {
+    const sessionCleared = clearSession(token);
+    if (sessionCleared) {
+      return true;
+    }
+
+    throw Error(sys_messages.error.CLEAR_SESSION_FAILED);
+  }
+
+  return sys_messages.warning.SESSION_NOT_FOUND;
+};
 
 module.exports = {
   login,
+  logout,
 };
--- API/Route/auth.js
+++ API/Route/auth.js
@@ -2,12 +2,13 @@ const Router = require("express").Router;
 const {
   createControllerActionMiddleware,
 } = require("../Middleware/createControllerActionMiddleware");
-const { login } = require("../Controller/authController");
+const { login, logout } = require("../Controller/authController");
 
 const router = Router();
 const basePath = "/auth";
 
 router.post("/login", createControllerActionMiddleware(login));
+router.post("/logout", createControllerActionMiddleware(logout));
 
 module.exports = {
   basePath,
