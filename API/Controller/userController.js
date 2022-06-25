24afeaa4f36f3bcf40870e25370619c324565069
sanank <sanankr@icloud.com>
2022-06-25 13:09:08 +0530

    handled case of existing user, send generic message for 500 and log the error

--- API/Controller/userController.js
+++ API/Controller/userController.js
@@ -1,18 +1,27 @@
-const { createUser: dbCreateUser } = require("../Service/userService");
+const { createUser: dbCreateUser, getUser } = require("../Service/userService");
 const { hash } = require("../Helper/encryption");
 const { sys_roles, sys_messages } = require("../config.json");
 
 const createUser = async (ctx) => {
   const { userName, password, role } = ctx.params;
 
+  if (userName && userName.trim() === "") {
+    throw ctx.Error(400, sys_messages.error.EMPTY_USER_NAME);
+  }
+
   // check password is only alpha numeric
   if (!(/[A-z]/.test(password) && /\d/.test(password))) {
-    throw ctx.Error(402, sys_messages.error.INVALID_PASSWORD_PATTERN);
+    throw ctx.Error(400, sys_messages.error.INVALID_PASSWORD_PATTERN);
   }
 
   // check role is recognised by system
   if (!Object.values(sys_roles).includes(role)) {
-    throw ctx.Error(402, sys_messages.error.INVALID_ROLE_REQUESTED);
+    throw ctx.Error(400, sys_messages.error.INVALID_ROLE_REQUESTED);
+  }
+
+  const existingUser = await getUser(userName);
+  if (existingUser) {
+    throw ctx.Error(403, sys_messages.error.USER_NAME_NOT_AVAILABLE);
   }
 
   await dbCreateUser({
